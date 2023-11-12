23fda42c7627c58af36e9793371bda33ce9d94cd
Owen <115508125+Gitowe@users.noreply.github.com>
2023-11-11 19:01:43 -0600

    update JWKS.py

--- JWKS-server-v2/JWKS.py
+++ JWKS-server-v2/JWKS.py
@@ -3,46 +3,59 @@ from datetime import datetime, timedelta
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 import jwt
+import base64
 
 app = Flask(__name__)
 
-# Generate RSA key pair and associate kid and expiry timestamp
-def generate_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
-    public_key = private_key.public_key()
 
-    private_key_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption()
-    )
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+expired_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+public_key = private_key.public_key()
 
-    public_key_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
+private_key_bytes = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+expired_key_bytes = expired_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption()
+)
+public_key_bytes = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+    
+numbers = private_key.private_numbers()
 
-    kid = str(datetime.utcnow().timestamp())
-    expiry = datetime.utcnow() + timedelta(days=1)
+kid = "normal"
+expkid = "expired"
 
-    return {
-        "kid": kid,
-        "exp": expiry,
-        "public_key": public_key_bytes.decode('utf-8')
-    }
+expiry = datetime.utcnow() + timedelta(hours=1)
 
-keys = [generate_key()]
+def int_to_base64(number):
+    byte_representation = number.to_bytes((number.bit_length() + 7) // 8, byteorder='big', signed=False)
+    base64url_encoded = base64.urlsafe_b64encode(byte_representation).decode('utf-8').rstrip('=')
+
+    return base64url_encoded
+
 
 @app.route('/.well-known/jwks.json', methods=['GET'])
 def jwks():
     if request.method == 'GET':
-        now = datetime.utcnow()
-        filtered_keys = [key for key in keys if now < key["exp"]]
         jwks = {
-            "keys": [{"kid": key["kid"], "kty": "RSA", "alg": "RS256", "use": "sig", "n": key["public_key"]} for key in filtered_keys]
+            "keys": [
+                {
+                    "kid": kid, "kty": "RSA", "alg": "RS256", "use": "sig", "n": int_to_base64(numbers.public_numbers.n), "e": int_to_base64(numbers.public_numbers.e)
+                }
+            ]
         }
         return jsonify(jwks)
     else:
@@ -52,18 +65,14 @@ def jwks():
 def authenticate():
     if request.method == 'POST':
         expired = request.args.get('expired')
-        key = keys[-1]  # Use the latest key (could be expired)
-        expiry = key["exp"]
-        private_key_for_signing = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
-        expired_key_for_signing = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048
-        )
         
-        token = jwt.encode({'exp': expiry, 'kid': key['kid']}, private_key_for_signing, algorithm='RS256')
+        headers={"kid": kid}
+        payload = {"user": "username","exp": expiry}
+        if expired == 'true':
+            headers["kid"] = expkid
+            payload["exp"] = datetime.utcnow() - timedelta(hours=1)
+        
+        token = jwt.encode(payload, private_key_bytes, algorithm='RS256', headers=headers)
         return token
     else:
         return jsonify({'message': 'Method Not Allowed'}), 405
